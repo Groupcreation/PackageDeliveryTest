57f021872b9e19d98f213aa1e58e8fdcb5f506d7
Groupcreation <68987062+Groupcreation@users.noreply.github.com>
2020-08-14 13:08:14 +0530

    Updated

--- PackagingDeliveryCharge.cs
+++ PackagingDeliveryCharge.cs
@@ -19,6 +19,9 @@ namespace Payment.Tests
         [TestCase("Integral", 2, 600)]
         [TestCase("Accessory", 2, 300)]
         [TestCase("Mirror", 2, 0)]
+        [TestCase("Inegral", 5, 0)]
+        [TestCase("Integral", -5, -1)]
+        [TestCase("Accessory", 0, -1)]
         public void Test_GetPackagingDeliveryCharge_Action(string item, int count, int charges)
         {
             GetPackagingDeliveryChargeController controller = new GetPackagingDeliveryChargeController();
